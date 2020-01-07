88463d253ce997ad95ae512f378208bec17712cd
Nicolas Boulet <nicolasboulet@live.fr>
2020-01-07 19:27:31 +0100

    modify routes and add a button to cocktails show

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,5 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create, :delete]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'cocktails#index'
 end
